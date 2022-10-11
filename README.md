a903517cecad5d4708408cb7c8bf012180ebddc7
Justin Chen <ch3njus@gmail.com>
2022-10-11 11:25:56 -0400

    Set specific parameters for password. Added examples and usage to README.

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # cuddly-succotash
 Generate passwords locally
+
+### Usage
+
+Set parameters in `config.ini` based on password requirements.
+
+**Configuration parameters**:
+```
+min	 		  (int) Minimum length of password
+numeric 	  (int) Number of numeric characters
+upper   	  (int) Number of uppercase characters
+lower   	  (int) Number of lowercase characters
+special  	  (int) Number of special characters
+special_chars (int) Number of special characters
+```
+
+**Basic usage**
+
+Example from Microsoft
+
+![example](./assets/example.png)
+
+```
+[DEFAULT]
+min = 16
+numeric = 2
+upper = 2
+lower = 2
+special = 3
+special_chars =
+```
+
+Run `python pwd-gen.py -c config`
+
+Example output:
+```
+16-length pwd:
+
+Tw?E,5c6NKU/&pav
+```
+
+To generate a password without characters constraints, leave all the character fields empty:
+
+```
+[DEFAULT]
+min = 64
+numeric =
+upper =
+lower =
+special =
+special_chars =
+```
+
+Example output:
+```
+64-length pwd:
+
+WiZF3p!Bc~1HY&PE;6^L9<shIXlN>{s7Sh*Pn`}cstE">l8tLI$iW5RA4IM.vTvQ
+```
+
+**Specific special characters**
+
+Add specific special characters that can be used as a single string without spaces or seperators. If not special characters are 
+specified, the punctuation characters from Python's string library will be used by default.
+
+```
+[DEFAULT]
+min = 16
+numeric = 2
+upper = 2
+lower = 2
+special = 3
+special_chars = !$
+```
--- pwd-gen.py
+++ pwd-gen.py
@@ -1,24 +1,91 @@
 '''
 Author: Justin Chen
-Date:   5.1.2020
+Date:   10.11.2022
 '''
 
-import string
 import random
 import argparse
+import configparser
+from string import ascii_lowercase, ascii_uppercase, digits, punctuation
 
-def generate_pwd(length, charset):
-  if length < 64: raise Exception('Password must be a minimum length of 64')
-  return ''.join(random.choice(charset) for _ in range(length))
+CHARACTER_SET = []
+CHARACTER_SET.extend(ascii_lowercase)
+CHARACTER_SET.extend(ascii_uppercase)
+CHARACTER_SET.extend(digits)
+
+
+'''
+inputs:
+min_length    (int)  Minimum number of characters
+num_numeric   (int)  Number of digits
+num_upper     (int)  Number of uppercase characters
+num_lower     (int)  Number of lowercase characters
+num_special	  (int)  Number of special characters
+special_chars (list) List of user specified special characters
+
+output:
+requirements (list) Randomly selected required characters
+'''
+def set_requirements(min_length, num_numeric, num_upper, num_lower, num_special, special_chars):
+	password = []
+
+	special = special_chars if special_chars else punctuation
+	CHARACTER_SET.extend(special)
+ 
+	if min_length < 8:
+		raise Exception('Invalid length: password must be greater than 8 characters')
+	
+	password.extend(random.choices(digits, k=num_numeric))
+	password.extend(random.choices(ascii_uppercase, k=num_upper))
+	password.extend(random.choices(ascii_lowercase, k=num_lower))
+	password.extend(random.choices(special, k=num_special))
+
+	return password
+
+
+'''
+inputs:
+password   (list) List of characters to be extended
+min_length (int)  Minimum length of the password
+
+output:
+password (list) List of all characters in the password
+'''
+def random_pad(password, min_length):
+	if len(password) < min_length:
+		remaining = min_length - len(password)
+		password.extend(random.choices(CHARACTER_SET, k=remaining))
+	
+	return password
+
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-l', '--length', type=int, default=64, help='password length (default: 64)')
+	format_filename = lambda file: f'{file}.ini' if not file.endswith('.ini') else file
+	
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-c', '--config', type=format_filename, required=True, help='Configuration file')
+	args = parser.parse_args()
 
-    charset = list(set(string.printable) - set(string.whitespace))
-    args = parser.parse_args()
-    pwd = generate_pwd(args.length, charset=charset)
-    print(f'{args.length}-length pwd:\n\n{pwd}\n')
+	config = configparser.ConfigParser(allow_no_value=True)
+	config.read(args.config)
+	cfg = config['DEFAULT']
+ 
+	toInt = lambda x: 0 if not x else int(x)
+ 
+	min_length = toInt(cfg['min'])
+	num_numeric = toInt(cfg['numeric'])
+	num_upper = toInt(cfg['upper'])
+	num_lower = toInt(cfg['lower'])
+	num_special = toInt(cfg['special'])
+	special_chars = cfg['special_chars'].split()
+  
+	password = set_requirements(min_length, num_numeric, num_upper, num_lower, num_special, special_chars)
+	password = random_pad(password, min_length)
+	random.shuffle(password)
+	password = ''.join(password)
+
+	print(f"\n{len(password)}-length pwd:\n\n{password}\n")
+
 
 if __name__ == '__main__':
   main()
